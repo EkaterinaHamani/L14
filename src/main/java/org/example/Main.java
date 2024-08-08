92ac37e356d46e3c77738c95895de0e69fe61cee
HamaniEkaterina <kat.vee.cs@gmail.com>
2024-08-08 15:50:59 +0300

    fix 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,48 +5,49 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
-        driver.get(" http://mts.by");
-    }
+        //4
+        driver.get("http://mts.by");
+        driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[1]/div[2]/form[1]/div[1]/input")).sendKeys("297777777");
+        driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[1]/div[2]/form[1]/div[2]/input")).sendKeys("50");
+        driver.findElement(By.cssSelector("#pay-connection > button")).click();
 
-    @Test
-    public void testTitle() {
-        WebDriver driver = new ChromeDriver();
-        WebElement blockTitle = driver.findElement(By.className("pay"));
-        String expectedTitle = "Онлайн пополнение без комиссии";
-        Assert.assertEquals(blockTitle.getText(), expectedTitle);
-    }
+        driver.get("http://mts.by");
 
-    @Test
-    public void testLogos() {
-        WebDriver driver = new ChromeDriver();
-        List<WebElement> logos = driver.findElements(By.className("pay_partners"));
-        Assert.assertTrue(logos.isEmpty(), "Логтипы платежных систем присутствуют");
-    }
+        //1
+        WebElement blockTitle = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/h2"));
+        String expectedTitle = "Онлайн пополнение\n" +
+                "без комиссии";
+        Assert.assertEquals(blockTitle.getText(), expectedTitle, "ок");
 
-    @Test
-    public void testLink() {
-        WebDriver driver = new ChromeDriver();
-        WebElement infoLink = driver.findElement(By.xpath("//*[@id='pay-section']/div/div/div[2]/section/div/a"));
+        //2
+        List<WebElement> logos = driver.findElements(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[2]"));
+        Assert.assertTrue(!logos.isEmpty(), "Логтипы платежных систем присутствуют");
+
+        //3
+        WebElement infoLink = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/a"));
         infoLink.click();
         String expectedUrl = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
         String actualURL = driver.getCurrentUrl();
-        Assert.assertEquals(actualURL, expectedUrl, "Подробнее о сервисе");
-    }
+        Assert.assertEquals(actualURL, expectedUrl, "ок");
+        driver.quit();
 
-    @Test
-    public void testButton() {
-        WebDriver driver = new ChromeDriver();
-        driver.findElement(By.xpath("//*[@id=\"connection-phone\"]")).sendKeys("297777777");
-        driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button")).click();
-    }
 
+    }
 }
+
+
+
+
+
+
+
 
